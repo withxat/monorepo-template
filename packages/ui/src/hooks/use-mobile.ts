c558456814db633c2c2c0c2d3dc358dddf71e886
Xat <i@xat.sh>
2026-07-01 16:37:52 +0800

    refactor(ui): use cnfast and foxact utilities

--- packages/ui/src/hooks/use-mobile.ts
+++ packages/ui/src/hooks/use-mobile.ts
@@ -1,19 +1,8 @@
-import * as React from 'react'
+import { useMediaQuery } from 'foxact/use-media-query'
 
 const MOBILE_BREAKPOINT = 768
 const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
-function subscribe(onStoreChange: () => void) {
-	const mediaQueryList = window.matchMedia(MOBILE_QUERY)
-	mediaQueryList.addEventListener('change', onStoreChange)
-
-	return () => mediaQueryList.removeEventListener('change', onStoreChange)
-}
-
-function getSnapshot() {
-	return window.matchMedia(MOBILE_QUERY).matches
-}
-
 export function useIsMobile() {
-	return React.useSyncExternalStore(subscribe, getSnapshot, () => false)
+	return useMediaQuery(MOBILE_QUERY, false)
 }
